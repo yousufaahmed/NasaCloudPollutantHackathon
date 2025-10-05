b474709d31b4107fc95632d45e683ee8ec8b026b
Delta <julio_15@hotmail.co.uk>
2025-10-05 03:17:12 +0100

    creating the weather widget

--- source/from-earthdata-to-action/src/app/home/page.tsx
+++ source/from-earthdata-to-action/src/app/home/page.tsx
@@ -1,3 +1,18 @@
 export default async function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <h1 className="text-4xl font-bold mb-4 text-center">
+        Welcome to Cleanview
+      </h1>
+      <div className="flex w-full px-[5%]">
+        <div className="card bg-base-300 rounded-box grid h-20 grow place-items-center">
+          content
+        </div>
+        <div className="divider divider-horizontal"></div>
+        <div className="card bg-base-300 rounded-box grid h-20 grow place-items-center">
+          content
+        </div>
+      </div>
+    </div>
+  );
 }
