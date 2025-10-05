6bce6028f4664ae8358c9e3a313ad0ca178b81e3
Delta <julio_15@hotmail.co.uk>
2025-10-05 06:05:53 +0100

    home page commit gotta check the look of the page

--- source/from-earthdata-to-action/src/app/home/page.tsx
+++ source/from-earthdata-to-action/src/app/home/page.tsx
@@ -4,13 +4,16 @@ export default async function Home() {
       <h1 className="text-4xl font-bold mb-4 text-center">
         Welcome to Cleanview
       </h1>
-      <div className="flex w-full px-[5%]">
-        <div className="card bg-base-300 rounded-box grid h-20 grow place-items-center">
-          content
+      <div className="flex w-full px-[5%] font-mono">
+        <div className="card bg-base-300 rounded-box grid h-40 grow place-items-center">
+          Breathe Easy with Cleanview!!
         </div>
         <div className="divider divider-horizontal"></div>
-        <div className="card bg-base-300 rounded-box grid h-20 grow place-items-center">
-          content
+        <div className="card bg-base-300 rounded-box grid h-40 grow place-items-center">
+          Cleanview helps you stay one step ahead for a healthier day. Using
+          TEMPO data, it smartly blends weather and local air info to give you
+          the clearest picture possible. Cleanview's got you covered with tech
+          thats fresh, and clean.
         </div>
       </div>
     </div>
--- source/from-earthdata-to-action/src/app/navbar.tsx
+++ source/from-earthdata-to-action/src/app/navbar.tsx
@@ -7,13 +7,13 @@ export default function Navbar() {
 
   navbarItems.push("Home", "Dashboard");
   return (
-    <nav className="w-full max-w-screen px-4 py-4 mx-auto flex top-0 left-0 z-50 menu-toggle justify-center">
+    <nav className="w-full max-w-screen px-4 py-2 mx-auto flex top-0 left-0 z-50 menu-toggle justify-center">
       <ul className="menu-links">
         {navbarItems.map((item) => (
           <Link
             key={item}
             href={`/${item.toLowerCase()}`}
-            className="text-3xl mx-2 text-black hover:text-gray-300 transition-colors duration-200 dark:text-white dark:hover:text-black dark:transition-colors duration-700 font-mono"
+            className="text-2xl mx-2 text-black hover:text-gray-300 transition-colors duration-200 dark:text-white dark:hover:text-black dark:transition-colors duration-700 font-mono"
           >
             {item}
           </Link>
